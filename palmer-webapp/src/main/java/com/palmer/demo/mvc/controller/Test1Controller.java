3ab92787801d400c4e7684a14f6055fcaeb888ff
xuechengju <xuechengju@wdai.com>
2018-02-24 11:24:21 +0800

    test1
    add a controller

---
+++ palmer-webapp/src/main/java/com/palmer/demo/mvc/controller/Test1Controller.java
@@ -0,0 +1,10 @@
+package com.palmer.demo.mvc.controller;
+
+/**
+ * @Author: xuechengju
+ * @Date: Created in 2018/2/24, at 上午11:16
+ * @Modified by:
+ * @Description:
+ */
+public class Test1Controller {
+}
